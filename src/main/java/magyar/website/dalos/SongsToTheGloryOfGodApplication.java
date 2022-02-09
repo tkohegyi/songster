5e8e576a505605336d884d8d9c6d9d8201e3ce58
tkohegyi <tamas_kohegyi@epam.com>
2022-02-09 22:02:49 +0100

    login logged properly

--- src/main/java/magyar/website/dalos/SongsToTheGloryOfGodApplication.java
+++ src/main/java/magyar/website/dalos/SongsToTheGloryOfGodApplication.java
@@ -1,27 +1,24 @@
 package magyar.website.dalos;
 
+import magyar.website.dalos.web.oauth2.LoginSuccessHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Collections;
-import java.util.Map;
 
 @SpringBootApplication
-@RestController
 public class SongsToTheGloryOfGodApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SongsToTheGloryOfGodApplication.class, args);
 	}
+
+	@Autowired
+	private LoginSuccessHandler loginSuccessHandler;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -30,12 +27,7 @@ public class SongsToTheGloryOfGodApplication extends WebSecurityConfigurerAdapte
 				.exceptionHandling(e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
 				.logout(l -> l.logoutSuccessUrl("/").permitAll())
 				.csrf(c -> c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
-				.oauth2Login();
+				.oauth2Login(o -> o.successHandler(loginSuccessHandler));
 		// @formatter:on
-	}
-
-	@GetMapping("/user")
-	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
-		return Collections.singletonMap("name", principal.getAttribute("name"));
 	}
 }
---
+++ src/main/java/magyar/website/dalos/web/oauth2/LoginSuccessHandler.java
@@ -0,0 +1,38 @@
+package magyar.website.dalos.web.oauth2;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class LoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+    static private final Logger logger = LoggerFactory.getLogger(LoginSuccessHandler.class);
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
+
+        DefaultOAuth2User userDetails = (DefaultOAuth2User) authentication.getPrincipal();
+        String method = ((OAuth2AuthenticationToken) authentication).getAuthorizedClientRegistrationId();
+        String id = userDetails.getName();
+        String name = userDetails.getAttribute("name");
+        String email = userDetails.getAttribute("email");
+        String picture = switch (method) {
+            case "google" -> userDetails.getAttribute("picture");
+            case "facebook" -> "empty avatar";
+            case "github" -> userDetails.getAttribute("avatar_url");
+            default -> throw new IllegalStateException("Unexpected value: " + method);
+        };
+
+        logger.info("The user/id: {}/{} has logged in by using: {}.", name, id, method);
+        super.onAuthenticationSuccess(request, response, authentication);
+    }
+}
